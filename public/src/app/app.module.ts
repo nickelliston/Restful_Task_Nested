0b24b755e9fe82905da92798893fc3a12489ebe4
Nick Elliston <nickelliston@gmail.com>
2019-09-19 19:43:52 -0700

    initial commit

---
+++ public/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './http.service';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TaskComponent } from './task/task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [HttpService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
